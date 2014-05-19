dcdcddf50a391cbc371222e3a5d447255e3b8f78
Jared Hancock <jared@osticket.com>
2014-05-19 13:49:17 -0500

    http: Implement pass-through auth for clients

--- auth-passthru/authenticate.php
+++ auth-passthru/authenticate.php
@@ -5,6 +5,10 @@ class HttpAuthentication extends StaffAuthenticationBackend {
     static $name = "HTTP Authentication";
     static $id = "passthru";
 
+    function supportsInteractiveAuthentication() {
+        return false;
+    }
+
     function signOn() {
         if (isset($_SERVER['REMOTE_USER']) && !empty($_SERVER['REMOTE_USER']))
             // User was authenticated by the HTTP server
@@ -30,8 +34,63 @@ class HttpAuthentication extends StaffAuthenticationBackend {
     }
 }
 
+class UserHttpAuthentication extends UserAuthenticationBackend {
+    static $name = "HTTP Authentication";
+    static $id = "passthru.client";
+
+    function supportsInteractiveAuthentication() {
+        return false;
+    }
+
+    function signOn() {
+        if (isset($_SERVER['REMOTE_USER']) && !empty($_SERVER['REMOTE_USER']))
+            // User was authenticated by the HTTP server
+            $username = $_SERVER['REMOTE_USER'];
+        elseif (isset($_SERVER['REDIRECT_REMOTE_USER'])
+                && !empty($_SERVER['REDIRECT_REMOTE_USER']))
+            $username = $_SERVER['REDIRECT_REMOTE_USER'];
+
+        if ($username) {
+            // Support ActiveDirectory domain specification with either
+            // "user@domain" or "domain\user" formats
+            if (strpos($username, '@') !== false)
+                list($username, $domain) = explode('@', $username, 2);
+            elseif (strpos($username, '\\') !== false)
+                list($domain, $username) = explode('\\', $username, 2);
+            $username = trim(strtolower($username));
+
+            if ($acct = ClientAccount::lookupByUsername($username)) {
+                if (($client = new ClientSession(new EndUser($acct->getUser())))
+                        && $client->getId())
+                    return $client;
+            }
+            else {
+                // No such account. Attempt a lookup on the username
+                $users = parent::searchUsers($username);
+                if (!is_array($users))
+                    return;
+
+                foreach ($users as $u) {
+                    if (0 === strcasecmp($u['username'], $username)
+                            || 0 === strcasecmp($u['email'], $username))
+                        // User information matches HTTP username
+                        return new ClientCreateRequest($this, $username, $u);
+                }
+            }
+        }
+    }
+}
+
+require_once(INCLUDE_DIR.'class.plugin.php');
+require_once('config.php');
 class PassthruAuthPlugin extends Plugin {
+    var $config_class = 'PassthruAuthConfig';
+
     function bootstrap() {
-        StaffAuthenticationBackend::register('HttpAuthentication');
+        $config = $this->getConfig();
+        if ($config->get('auth-staff'))
+            StaffAuthenticationBackend::register('HttpAuthentication');
+        if ($config->get('auth-client'))
+            UserAuthenticationBackend::register('UserHttpAuthentication');
     }
 }
