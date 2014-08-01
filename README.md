e6c495c815d1438332e9ab444218632e8f0cc16d
Jared Hancock <jared@osticket.com>
2014-08-01 14:05:57 -0500

    Add some initial docs concerning translations

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ After cloning, `hydrate` the repo by downloading the third-party library
 dependencies.
 
     php make.php hydrate
+
+Translating
+===========
+
+[![Crowdin](https://d322cqt584bo4o.cloudfront.net/osticket-plugins/localized.png)](http://i18n.osticket.com/project/osticket-plugins)
+
+Translation service is being performed via the Crowdin translation
+management software. The project page for the plugins is located at
+
+http://i18n.osticket.com/projects/osticket-plugins
--- auth-oauth/plugin.php
+++ auth-oauth/plugin.php
@@ -6,7 +6,7 @@ return array(
     'name' =>           /* trans */ 'Oauth2 Authentication and Lookup',
     'author' =>         'Jared Hancock',
     'description' =>    /* trans */ 'Provides a configurable authentication backend
-        for authenticating staff and clients using an OATH2 server
+        for authenticating staff and clients using an OAUTH2 server
         interface.',
     'url' =>            'http://www.osticket.com/plugins/auth/oauth',
     'plugin' =>         'authentication.php:OauthAuthPlugin',
