2c325c2c80c58d23054007f51c62e91e1054c5d8
Niko <niko@nkdev.io>
2020-04-23 10:11:22 +0400

    first version of hcaptcha field

---
+++ field-hcaptcha/plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'id' =>             'field:hCaptcha', # notrans
+    'version' =>        '0.1',
+    'name' =>           'Cloudflare hCaptcha field',
+    'author' =>         'Niko',
+    'description' =>    'Provides a hCaptcha field. This will help verify humans for both new
+    tickets by guests as well as client registration. Loosely based on the reCaptcha Plugin by Jared',
+    'url' =>            'http://www.osticket.com/plugins/field/hcaptcha',
+    'plugin' =>         'field.php:hCaptchaPlugin',
+);
