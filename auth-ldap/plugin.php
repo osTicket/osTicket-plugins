991c804c3f361958d154df79827487a2cae41465
unknown <kallibr44@gmail.com>
2020-08-03 01:36:33 +0700

    modified staff authorize

--- auth-ldap/plugin.php
+++ auth-ldap/plugin.php
@@ -2,9 +2,9 @@
 set_include_path(get_include_path().PATH_SEPARATOR.dirname(__file__).'/include');
 return array(
     'id' =>             'auth:ldap', # notrans
-    'version' =>        '0.6.2.1',
+    'version' =>        '0.6.2',
     'name' =>           /* trans */ 'LDAP Authentication and Lookup',
-    'author' =>         'Jared Hancock & modified by kallibr44',
+    'author' =>         'Jared Hancock',
     'description' =>    /* trans */ 'Provides a configurable authentication backend
         which works against Microsoft Active Directory and OpenLdap
         servers',
