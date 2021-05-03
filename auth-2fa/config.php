3b26e9754282f8f2c86402a74d0996518154ae93
aydreeihn <adriane@enhancesoft.com>
2021-05-03 12:56:30 -0500

    Authenticator Plugin
    This commit adds the Authenticator Plugin to the osTicket Plugins repo. The wording of the Plugin has been changed to specify that it works with other Authenticator apps, and when a QR code is scanned, the title shown in the app is the Helpdesk title.

---
+++ auth-2fa/config.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once INCLUDE_DIR . 'class.plugin.php';
+
+class Auth2FAConfig extends PluginConfig {
+
+    // Provide compatibility function for versions of osTicket prior to
+    // translation support (v1.9.4)
+    function translate() {
+        if (!method_exists('Plugin', 'translate')) {
+            return array(
+                function($x) { return $x; },
+                function($x, $y, $n) { return $n != 1 ? $y : $x; },
+            );
+        }
+        return Plugin::translate('2fa-auth');
+    }
+}
