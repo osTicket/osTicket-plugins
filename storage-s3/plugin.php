78bd732b09a66d3b89724d676d942da36053babd
JediKev <kevin@enhancesoft.com>
2022-01-25 21:21:00 +0000

    issue: Latest aws-sdk-php
    This updates the storage-s3 plugin to utilize latest aws-sdk-php.

--- storage-s3/plugin.php
+++ storage-s3/plugin.php
@@ -2,19 +2,23 @@
 
 return array(
     'id' =>             'storage:s3',
-    'version' =>        '0.2.1',
+    'version' =>        '0.3',
     'name' =>           /* trans */ 'Attachments hosted in Amazon S3',
-    'author' =>         'Jared Hancock',
+    'author' =>         'Jared Hancock, Kevin Thorne',
     'description' =>    /* trans */ 'Enables storing attachments in Amazon S3',
     'url' =>            'http://www.osticket.com/plugins/storage-s3',
     'requires' => array(
         "aws/aws-sdk-php" => array(
-            'version' => "2.*",
+            'version' => "3.*",
             'map' => array(
-                'aws/aws-sdk-php/src/Aws/S3' => 'lib/Aws/S3',
-                'aws/aws-sdk-php/src/Aws/Common' => 'lib/Aws/Common',
-                'guzzle/guzzle/src/Guzzle' => 'lib/Guzzle',
-                'symfony/event-dispatcher' => 'lib/Symfony/Component/EventDispatcher',
+                'aws/aws-sdk-php/src' => 'lib/Aws',
+                'guzzlehttp/guzzle/src' => 'lib/GuzzleHttp',
+                'guzzlehttp/promises/src' => 'lib/GuzzleHttp/Promise',
+                'guzzlehttp/psr7/src/' => 'lib/GuzzleHttp/Psr7',
+                'mtdowling/jmespath.php/src' => 'lib/JmesPath',
+                'psr/http-client/src' => 'lib/Psr/Http/Client',
+                'psr/http-factory/src' => 'lib/Psr/Http/Factory',
+                'psr/http-message/src' => 'lib/Psr/Http/Message',
             ),
         ),
     ),
