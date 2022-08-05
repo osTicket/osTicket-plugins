9346b14f18168a0301b7e470e6d5df0106da810c
Peter Rotich <peter@osticket.com>
2022-08-05 04:48:40 +0000

    auth-oauth2: Change version to 0.3

--- auth-oauth2/plugin.php
+++ auth-oauth2/plugin.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'id' =>             'auth:oath2', # notrans
-    'version' =>        '0.2',
+    'version' =>        '0.3',
     'ost_version' =>    '1.17', # Require osTicket v1.17+
     'name' =>           /* trans */ 'Oauth2 Client',
     'author' =>         'Peter Rotich <peter@osticket.com>',
